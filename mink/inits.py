a35d8c80f90451114f95513948033dd352052f6b
Benjamin Bossan <benjamin.bossan@gmail.com>
2016-05-25 16:11:10 +0200

    Rename some units, add Glorot.

--- mink/inits.py
+++ mink/inits.py
@@ -1,3 +1,4 @@
+import numpy as np
 from sklearn.base import BaseEstimator
 import tensorflow as tf
 
@@ -13,28 +14,74 @@ __all__ = [
 
 
 class Init(BaseEstimator):
-    def __call__(self, *args, **kwargs):
+    def __call__(self, shape):
         raise NotImplementedError
 
 
-class InitNormal(Init):
+class Constant(Init):
     def __init__(
             self,
-            mean=0.0,
-            stddev=1.0,
+            val=0.0,
     ):
+        self.val = val
+
+    def __call__(self, shape):
+        return tf.Variable(self.val * tf.ones(shape=shape))
+
+
+class Uniform(Init):
+    def __init__(
+            self,
+            range=0.01,
+            std=None,
+            mean=0.0,
+            seed=None,
+    ):
+        self.range = range
+        self.std = std
         self.mean = mean
-        self.stddev = stddev
+        self.seed = seed
+
+    def __call__(self, shape):
+        range, std, mean = self.range, self.std, self.mean
+        if std is not None:
+            high = mean - np.sqrt(3) * std
+            low = mean + np.sqrt(3) * std
+        else:
+            try:
+                high, low = range  # range is a tuple
+            except TypeError:
+                high, low = -range, range  # range is a number
+
+        return tf.Variable(tf.random_uniform(
+            shape=shape,
+            minval=low,
+            maxval=high,
+            seed=self.seed,
+        ))
+
+
+class Normal(Init):
+    def __init__(
+            self,
+            std=1.0,
+            mean=0.0,
+            seed=None,
+    ):
+        self.std = std
+        self.mean = mean
+        self.seed = seed
 
     def __call__(self, shape):
         return tf.Variable(tf.random_normal(
             shape=shape,
+            stddev=self.std,
             mean=self.mean,
-            stddev=self.stddev,
+            seed=self.seed,
         ))
 
 
-class InitTruncatedNormal(Init):
+class TruncatedNormal(Init):
     def __init__(
             self,
             mean=0.0,
@@ -51,8 +98,57 @@ class InitTruncatedNormal(Init):
         ))
 
 
-class InitZeros(Init):
+class Zeros(Init):
     def __call__(self, shape):
         return tf.Variable(tf.zeros(
             shape=shape,
         ))
+
+
+class Glorot(Init):
+    def __init__(
+            self,
+            initializer,
+            gain=np.sqrt(2),
+            c01b=False,
+    ):
+        self.initializer = initializer
+        self.gain = gain
+        self.c01b = c01b
+
+    def __call__(self, shape):
+        if self.c01b:
+            if len(shape) != 4:
+                raise RuntimeError(
+                    "If c01b is True, only shapes of length 4 are accepted")
+
+            n1, n2 = shape[0], shape[3]
+            receptive_field_size = shape[1] * shape[2]
+        else:
+            if len(shape) < 2:
+                raise RuntimeError(
+                    "This initializer only works with shapes of length >= 2")
+
+            n1, n2 = shape[:2]
+            receptive_field_size = np.prod(shape[2:])
+
+        std = self.gain * np.sqrt(2.0 / ((n1 + n2) * receptive_field_size))
+        return self.initializer(std=std)(shape)
+
+
+class GlorotNormal(Glorot):
+    """Glorot with weights sampled from the Normal distribution.
+
+    See :class:`Glorot` for a description of the parameters.
+    """
+    def __init__(self, gain=1.0, c01b=False):
+        super().__init__(Normal, gain, c01b)
+
+
+class GlorotUniform(Glorot):
+    """Glorot with weights sampled from the Uniform distribution.
+
+    See :class:`Glorot` for a description of the parameters.
+    """
+    def __init__(self, gain=1.0, c01b=False):
+        super().__init__(Uniform, gain, c01b)
