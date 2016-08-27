d5b79430375797f52f41feefb13463bf71f39e1c
BenjaminBossan <benjamin.bossan@gmail.com>
2016-08-27 19:01:46 +0200

    Add Adagrad.

--- mink/updates.py
+++ mink/updates.py
@@ -8,6 +8,7 @@ __all__ = [
     'SGD',
     'Momentum',
     'Adam',
+    'Adagrad',
 ]
 
 
@@ -45,6 +46,17 @@ class Momentum(Update):
 
 class Adam(Update):
     def __init__(self, learning_rate=1e-4):
+        self.learning_rate = learning_rate
+
+    def __call__(self, loss):
+        train_step = tf.train.AdadeltaOptimizer(
+            learning_rate=self.learning_rate,
+        ).minimize(loss)
+        return train_step
+
+
+class Adagrad(Update):
+    def __init__(self, learning_rate=1.0):
         self.learning_rate = learning_rate
 
     def __call__(self, loss):
