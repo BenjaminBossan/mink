c0313f49b655699b349b9414670eb30f84505cb5
BenjaminBossan <benjamin.bossan@gmail.com>
2016-08-28 12:31:12 +0200

    Add parameters to some updates, add RMSProp.

--- mink/updates.py
+++ mink/updates.py
@@ -45,12 +45,21 @@ class Momentum(Update):
 
 
 class Adam(Update):
-    def __init__(self, learning_rate=1e-4):
+    def __init__(
+            self,
+            learning_rate=1e-4,
+            beta1=0.9,
+            beta2=0.999,
+    ):
         self.learning_rate = learning_rate
+        self.beta1 = beta1
+        self.beta2 = beta2
 
     def __call__(self, loss):
         train_step = tf.train.AdadeltaOptimizer(
             learning_rate=self.learning_rate,
+            beta1=self.beta1,
+            beta2=self.beta2,
         ).minimize(loss)
         return train_step
 
@@ -62,5 +71,25 @@ class Adagrad(Update):
     def __call__(self, loss):
         train_step = tf.train.AdadeltaOptimizer(
             learning_rate=self.learning_rate,
+        ).minimize(loss)
+        return train_step
+
+
+class RMSProp(Update):
+    def __init__(
+            self,
+            learning_rate=1.0,
+            decay=0.9,
+            momentum=0.0,
+    ):
+        self.learning_rate = learning_rate
+        self.decay = decay
+        self.momentum = momentum
+
+    def __call__(self, loss):
+        train_step = tf.train.RMSPropOptimizer(
+            learning_rate=self.learning_rate,
+            decay=self.decay,
+            momentum=self.momentum
         ).minimize(loss)
         return train_step
